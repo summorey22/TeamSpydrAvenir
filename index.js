0d942558c2ea0ed33c307df13b79d018ec47ee0c
summorey22 <sumitmorey247@gmail.com>
2021-09-26 17:16:40 +0530

    Co-authored-by: Atharva Deepak Naik <atharvanaik14@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var toggle_switch = document.getElementById('flexSwitchCheckDefault');
 function changeTheme() {
-    console.log(toggle_switch.checked);
     if(toggle_switch.checked) {
         document.body.style.setProperty('background-color', '#FFFFFF', 'important');
         document.getElementById('zali').style.setProperty('background-color', '#272727', 'important');
@@ -12,4 +11,90 @@ function changeTheme() {
     }
 }
 
+function init() {
+    var p = document.getElementById('1');
+    var style = window.getComputedStyle(p);
+    p.style.setProperty('background-color', '#FFFFFF', 'important');
+    document.getElementById('progresso').style.setProperty('width', (p.getBoundingClientRect().left - document.getElementById('progresso').getBoundingClientRect().left + 2) + "px", 'important');
+}
+
+function baap(numby) {
+    switch(numby)
+    {
+        case 1: {
+            var p = document.getElementById('1');
+            var style = window.getComputedStyle(p);
+            p.style.setProperty('background-color', '#FFFFFF', 'important');
+            document.getElementById('progresso').style.setProperty('width', (p.getBoundingClientRect().left - document.getElementById('progresso').getBoundingClientRect().left + 2) + "px", 'important');
+            for( let i = 2; i <=5 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#161616', 'important');
+            }
+            break;
+        }
+
+        case 2: {
+            var p = document.getElementById('2');
+            var style = window.getComputedStyle(p);
+            document.getElementById('progresso').style.setProperty('width', (p.getBoundingClientRect().left - document.getElementById('progresso').getBoundingClientRect().left + 2) + "px", 'important');
+            for( let i = 1; i <=2 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#FFFFFF', 'important');
+            }
+            for( let i = 3; i <=5 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#161616', 'important');
+            }
+            break;
+        }
+
+        case 3: {
+            var p = document.getElementById('3');
+            var style = window.getComputedStyle(p);
+            document.getElementById('progresso').style.setProperty('width', (p.getBoundingClientRect().left - document.getElementById('progresso').getBoundingClientRect().left + 2) + "px", 'important');
+            for( let i = 1; i <=3 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#FFFFFF', 'important');
+            }
+            for( let i = 4; i <=5 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#161616', 'important');
+            }
+            break;
+        }
+
+        case 4: {
+            var p = document.getElementById('4');
+            var style = window.getComputedStyle(p);
+            document.getElementById('progresso').style.setProperty('width', (p.getBoundingClientRect().left - document.getElementById('progresso').getBoundingClientRect().left + 2) + "px", 'important');
+            for( let i = 1; i <=4 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#FFFFFF', 'important');
+            }
+            document.getElementById("5").style.setProperty('background-color', '#161616', 'important');
+            break;
+        }
+
+        case 5: {
+            var p = document.getElementById('5');
+            var style = window.getComputedStyle(p);
+            document.getElementById('progresso').style.setProperty('width', (p.getBoundingClientRect().left - document.getElementById('progresso').getBoundingClientRect().left + 2) + "px", 'important');
+            for( let i = 1; i <=5 ; i++){
+                document.getElementById(i + "").style.setProperty('background-color', '#FFFFFF', 'important');
+            }
+            break;
+        }
+    }
+}
+
+/*
+function atharva(){
+    return '2%';
+}
+*/
+
 toggle_switch.addEventListener('click', changeTheme);
+
+/*
+a = progress bar left window length
+b = dot left window
+c = bar length
+
+(b-a/c)*100
+
+
+*/
